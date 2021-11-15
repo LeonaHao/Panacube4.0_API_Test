f727e348c251f7465de7e3259bd9c4d977c9ede5
Leona <393273765@qq.com>
2021-11-15 15:25:25 +0800

    增加共享创建与删除脚本

--- ToolsForSE/share.py
+++ ToolsForSE/share.py
@@ -7,68 +7,144 @@ import requests
 import urllib3
 from configs import urlConfigs
 from lib.PanaCubeCommon import login
+from random import randint
 
 
 
-
+Token = login()
 
 '''创建共享'''
-def createShare(poolId,num):
-    projectInfo = getProjectInfo()
-    projectId = projectInfo[int(poolId)][0]
-    projectName = projectInfo[int(poolId)][1]
+def createShare(num, projectId, projectName,type):
 
     headers ={
         "Content-Type":"application/json",
-        "Authorization": login(),
-        "POOL-ID": poolId,
-        "PROJECT-ID": projectId,
-        "PROJECT-NAME": projectName
-
+        "Authorization": Token
     }
     reqUrl = urlConfigs.shareUrl
-
-    volumePathList = getVolumePathList(poolId)
-    for volumePath in volumePathList:
-        LV = volumePath.split('/',2)[2]
+    if type == 'cn':
         reqParam = {
-            "name":"share"+ str(num)+LV,
-            "volume":volumePath,
-            "cifs":"yes",
-            "nfs":"yes",
-            "browseable":"yes",
-            "read_only":"no",
-            "create_mask":"0744",
-            "directory_mask":"0755",
-            "guest_ok":"yes",
-            "sync":"no",
-            "no_root_squash":"yes"
-        }
-        print("请求参数是:  {}".format(reqParam))
-        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-        result = requests.post(url=reqUrl, headers=headers,json=reqParam,verify=False).json()
-        print("响应结果是:  {}".format(result))
-        if result['message']=='success':
-            print("*******************成功创建共享**************************")
-        else:
-            print("*******************创建共享失败，响应结果为：{}".format(result))
+                    "name":"cAndNshare"+str(num),
+                    "cifs":"yes",
+                    "nfs":"yes",
+                    "s3":"no",
+                    "browseable":"yes",
+                    "read_only":"no",
+                    "create_mask":"0744",
+                    "directory_mask":"0755",
+                    "guest_ok":"yes",
+                    "sync":"no",
+                    "no_root_squash":"yes",
+                    "auth_enable":"disable",
+                    "compression":"off",
+                    "dedup":"off",
+                    "volblocksize":"8K",
+                    "share_type":0,
+                    "agree_type":[
+                        "CIFS",
+                        "NFS"
+                    ],
+                    "project_id":projectId,
+                    "project_name":projectName
+                }
+    elif type == 'c':
+        reqParam ={
+                "name":"cShare" + str(num),
+                "cifs":"yes",
+                "nfs":"no",
+                "s3":"no",
+                "browseable":"yes",
+                "read_only":"no",
+                "create_mask":"0744",
+                "directory_mask":"0755",
+                "guest_ok":"yes",
+                "sync":"no",
+                "no_root_squash":"no",
+                "auth_enable":"disable",
+                "compression":"off",
+                "dedup":"off",
+                "volblocksize":"8K",
+                "share_type":0,
+                "agree_type":[
+                    "CIFS"
+                ],
+                "project_id":projectId,
+                "project_name":projectName
+            }
+    elif type == 'n':
+        reqParam ={
+                "name":"nShare"+str(num),
+                "cifs":"no",
+                "nfs":"yes",
+                "s3":"no",
+                "browseable":"yes",
+                "read_only":"no",
+                "create_mask":"0744",
+                "directory_mask":"0755",
+                "guest_ok":"no",
+                "sync":"no",
+                "no_root_squash":"yes",
+                "auth_enable":"disable",
+                "compression":"off",
+                "dedup":"off",
+                "volblocksize":"8K",
+                "share_type":0,
+                "agree_type":[
+                    "NFS"
+                ],
+                "project_id":projectId,
+                "project_name":projectName
+            }
+    elif type == 's':
+        reqParam = {
+                "name":"s" + str(num),
+                "cifs":"no",
+                "nfs":"no",
+                "s3":"yes",
+                "browseable":"yes",
+                "read_only":"no",
+                "create_mask":"0744",
+                "directory_mask":"0755",
+                "guest_ok":"no",
+                "sync":"no",
+                "no_root_squash":"no",
+                "auth_enable":"disable",
+                "port": str(randint(9000,10000)),
+                "access_key":"123456",
+                "secret_key":"12345678",
+                "compression":"off",
+                "dedup":"off",
+                "volblocksize":"8K",
+                "share_type":2,
+                "agree_type":[
+                    "S3"
+                ],
+                "project_id":projectId,
+                "project_name":projectName
+            }
+    else:
+        print("参数类型错误，请退出重填！")
+
+
+    print("请求参数是:  {}".format(reqParam))
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    result = requests.post(url=reqUrl, headers=headers,json=reqParam,verify=False).json()
+    print("响应结果是:  {}".format(result))
+    if result['message']=='success':
+        print("*******************成功创建共享**************************")
+    else:
+        print("*******************创建共享失败，响应结果为：{}".format(result))
 
 '''批量共享'''
-def batchCreateShare(n, poolId,num):
+def batchCreateShare(n, num,projectId, projectName,type):
     for i in range(n):
-        createShare(poolId,num)
+        createShare(num,projectId, projectName,type)
         num = num + 1
 
 '''获取共享列表'''
 def getShareList(projectId,projectName):
-    # projectInfo = getProjectInfo()
-    # projectId = projectInfo[int(poolId)][0]
-    # projectName = projectInfo[int(poolId)][1]
-
     headers ={
         "Content-Type":"application/json",
-        "Authorization": login(),
-        # "POOL-ID": poolId,
+        "Authorization": Token,
         "PROJECT-ID": projectId,
         "PROJECT-NAME": projectName
 
@@ -94,20 +170,14 @@ def getShareList(projectId,projectName):
 
 '''删除共享列表'''
 def delShare(projectId,projectName):
-    # projectInfo = getProjectInfo()
-    # projectId = projectInfo[int(poolId)][0]
-    # projectName = projectInfo[int(poolId)][1]
-
     headers ={
         "Content-Type":"application/json",
-        "Authorization": login(),
-        # "POOL-ID": poolId,
+        "Authorization": Token,
         "PROJECT-ID": projectId,
         "PROJECT-NAME": projectName
     }
 
     shareIdList = getShareList(projectId,projectName)
-
     if len(shareIdList) > 0:
         reqUrl = urlConfigs.shareDelUrl
         reqParam = {
@@ -120,12 +190,14 @@ def delShare(projectId,projectName):
             print("*******************共享列表删除成功**************************")
         else:
             print("*******************共享列表删除失败,接口返回结果为：{}".format(shareDelRes))
+    else:
+        print("*******************共享列表为空，无需删除**************************")
 
 
 
 
 
-# createShare("40",2)
-# batchCreateShare(10,"676",1)
-# getShareList("bd355250753e40c890c2e00472c5ce5a","%E9%BB%98%E8%AE%A4%E6%B1%A0")
-# delShare("bd355250753e40c890c2e00472c5ce5a","%E9%BB%98%E8%AE%A4%E6%B1%A0")
+# createShare(2,"1732f00b451a48eb8ee65a7362dbb5dc","默认池","s")
+# batchCreateShare(3,3,"1732f00b451a48eb8ee65a7362dbb5dc","默认池","n")
+# getShareList("1732f00b451a48eb8ee65a7362dbb5dc","%E9%BB%98%E8%AE%A4%E6%B1%A0")
+# delShare("1732f00b451a48eb8ee65a7362dbb5dc","%E9%BB%98%E8%AE%A4%E6%B1%A0")
